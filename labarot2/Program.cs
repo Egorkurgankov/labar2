fbf613d21380af23a8c5f2728c728976c6e99c20
Egorkurgankov <egorkurgankov91@gmail.com>
2023-05-23 22:30:27 +0600

    v2

---
+++ labarot2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Conditional
+{
+    class SwitchCase
+    {
+        public static void Main(string[] args)
+        {
+            char op;
+            double first, second;
+
+            Console.Write("Введите первое число: ");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            second = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите оператор (+, -, *, /): ");
+            op = (char)Console.Read();
+
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine(first + second);
+                    break;
+
+                case '-':
+                    Console.WriteLine(first - second);
+                    break;
+
+                case '*':
+                    Console.WriteLine(first + second);
+                    break;
+
+                case '/':
+                    Console.WriteLine(first / second);
+                    break;
+
+                default:
+                    Console.WriteLine("Недопустимый оператор");
+                    break;
+
+            }
+        }
+    }
+}
